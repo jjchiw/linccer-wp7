6e6b85d3d274c59fd301abe3844c09077872fc5d
Juan J. Chiw <jjchiw@gmail.com>
2012-02-27 23:53:58 +0100

    Se cambio la libreria de linccer para que utilizara restsharp, solo envia y el gps esta hardcoded, solo envia todavia no recibe info

---
+++ linccerwp7/linccerwp7/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Device.Location;
+using Linccerwp7.Tasks;
+using System.Threading;
+
+namespace Linccerwp7
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		GeoCoordinateWatcher watcher;
+		LinccerTasks linccerTasks;
+		bool trackingOn = false;
+
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+			linccerTasks = new LinccerTasks();
+
+			watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High); // using high accuracy;
+			watcher.MovementThreshold = 10.0f; // meters of change before "PositionChanged"
+			// wire up event handlers
+			watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged);
+			watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+			
+			// start up LocServ in bg; watcher_StatusChanged will be called when complete.
+			new Thread(startLocServInBackground).Start();
+			StatusTextBlock.Text = "Starting Location Service...";
+
+		}
+
+		private void SendButton_Click(object sender, EventArgs e)
+		{
+			linccerTasks.Send(ContentTextBox.Text, (content) =>
+				{
+					Dispatcher.BeginInvoke(() => ResponseContentTextBlock.Text = content);
+				});
+		}
+
+		private void ReceiveButton_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		void watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+		{
+			switch (e.Status)
+			{
+				case GeoPositionStatus.Disabled:
+					trackingOn = false;
+					// The Location Service is disabled or unsupported.
+					// Check to see if the user has disabled the Location Service.
+					if (watcher.Permission == GeoPositionPermission.Denied)
+					{
+						// The user has disabled the Location Service on their device.
+						StatusTextBlock.Text = "You have disabled Location Service.";
+					}
+					else
+					{
+						StatusTextBlock.Text = "Location Service is not functioning on this device.";
+					}
+					break;
+				case GeoPositionStatus.Initializing:
+					trackingOn = true;
+					StatusTextBlock.Text = "Location Service is retrieving data...";
+					// The Location Service is initializing.
+					break;
+				case GeoPositionStatus.NoData:
+					// The Location Service is working, but it cannot get location data.
+					StatusTextBlock.Text = "Location data is not available.";
+					break;
+				case GeoPositionStatus.Ready:
+					trackingOn = true;
+					// The Location Service is working and is receiving location data.
+					StatusTextBlock.Text = "Location data is available.";
+					break;
+			}
+		}
+
+		void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+		{
+			// update the Map if the user has asked to be tracked.
+			if (trackingOn)
+			{
+				UpdateButton_Click(null, null);
+			}
+				
+		}
+
+		void startLocServInBackground()
+		{
+			watcher.TryStart(true, TimeSpan.FromMilliseconds(60000));
+		}
+
+		private void UpdateButton_Click(object sender, EventArgs e)
+		{
+			linccerTasks.UpdateLocation(watcher.Position.Location, (content) =>
+			{
+				Dispatcher.BeginInvoke(() => ResponseContentTextBlock.Text = content);
+			});
+		}
+	}
+}
---
+++ linccerwp7/linccerwp7/Tasks/LinccerTasks.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using LinccerApi;
+using Linccerwp7.Tos;
+using System.Device.Location;
+
+namespace Linccerwp7.Tasks
+{
+	public class LinccerTasks
+	{
+		private ClientConfig _config;
+		private Linccer _linccer;
+
+		public LinccerTasks()
+		{
+			this._config = new ClientConfig("C# Hoccer Demo");
+			this._config.UseProductionServers(); // enables communication to real Hoccer Clients (iOS & Android)
+
+			this._linccer = new Linccer();
+			this._linccer.Config = this._config;
+
+			
+		}
+
+		public void UpdateLocation(GeoCoordinate location, LinccerContentCallback callback)
+		{
+			// set geo position (must be changed to work on other locations than Molkenmarkt 2 in Berlin, Germany)
+			//41.38690, 2.16532
+			//41.386843, 2.165176
+			//this._linccer.Gps = new LocationInfo { Latitude = location.Latitude, Longitude = location.Longitude, Accuracy = 1000 };
+			this._linccer.Gps = new LocationInfo { Latitude = 41.386843, Longitude = 2.165176, Accuracy = 1000 };
+			this._linccer.SubmitEnvironment(callback);
+		}
+
+		public void Send(string content, LinccerContentCallback callback)
+		{
+			// inialize filecache for temporary up- and downloading large files (not used jet)
+			var cache = new FileCache();
+			cache.Config = this._config;
+
+			// create a plain message
+			Hoc hoc = new Hoc();
+			hoc.DataList.Add(
+				new HocData { Content = content, Type = "text/plain" }
+			);
+
+			// share it 1:1, in the Hoccer mobile App, you need to perform a drag in
+			// gesture to receive the message  (one-to-many is throw/catch)
+			this._linccer.Share("one-to-one", hoc, callback);
+		}
+
+		public void Receive()
+		{
+
+			// inialize filecache for temporary up- and downloading large files (not used jet)
+			var cache = new FileCache();
+			cache.Config = this._config;
+
+			// receive 1:1, in the Hoccer mobile App, you need to perform a drag out
+				// gesture to send something to this client (one-to-many is throw/catch)
+			this._linccer.Receive<Hoc>("one-to-one", (hoc) =>
+			{
+				if (hoc == null)
+					System.Console.WriteLine("no sender found");
+				else
+					System.Console.WriteLine(hoc);
+			});
+		}
+
+		
+	}
+}
